3f7d0ae961f528fb9b44ec880615793c06dedcd7
FherStk <porrino.fernando@gmail.com>
2023-09-27 09:59:37 +0000

    Study data and form has been updated (new fields, required ones, etc.)

--- models/study.py
+++ models/study.py
@@ -3,16 +3,19 @@
 from odoo import models, fields, api
 
 class ims_study(models.Model):
-    _name = 'ims.study'
-    _description = 'Study: The concrete type of stidy (kind of bachelor, concrete univeristy grade, etc.)'
+    _name = "ims.study"
+    _description = "Study: The concrete type of stidy (kind of bachelor, concrete univeristy grade, etc.)"
     
-    code = fields.Char('Code')
-    acronym = fields.Char('Acronym')
-    name = fields.Char('Name')
-    date = fields.Date('Release Date')
-    deprecated = fields.Boolean('Deprecated')
-    notes = fields.Text('Notes')
+    code = fields.Char(string="Code", required="true")
+    acronym = fields.Char(string="Acronym", required="true")
+    name = fields.Char(string="Name", required="true")
+    date = fields.Date(string="Release Date", required="true")
+    deprecated = fields.Boolean(string="Deprecated", required="true")
+    main_decree = fields.Binary(string="Main decree")
+    regional_decree = fields.Binary(string="Regional decree")
+    notes = fields.Text(string="Notes")
 
     professional_modules = fields.One2many(comodel_name="ims.professional_module", inverse_name="study", string="Professional Modules")
     follows = fields.One2many(comodel_name="ims.tracking", inverse_name="study", string="Follow-up")
+    level = fields.Many2one(comodel_name="ims.level", string="Level")
 
