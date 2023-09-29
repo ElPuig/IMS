e6d2222272df000fa328dc89ab8db44677cb2bc3
FherStk <porrino.fernando@gmail.com>
2023-09-29 11:10:50 +0000

    Fields reordered, PMs grouped, Group form improved

--- models/group.py
+++ models/group.py
@@ -3,12 +3,19 @@
 from odoo import models, fields, api
 
 class ims_group(models.Model):
-	_name = 'ims.group'
-	_description = 'Groups: Where the students are assigned to.'
+	_name = "ims.group"
+	_description = "Groups: Where the students are assigned to."	
+	
+	course = fields.Integer(string="Course", required="true")
+	acronym = fields.Char(string="Acronym", required="true")
+	# name = fields.Char(string="Name")
+	name=fields.Char(string='Name',compute='_compute_name')
+	notes = fields.Text(string="Notes")
 
-	acronym = fields.Char('Acronym')
-	name = fields.Char('Name')
-	email = fields.Char('Email')	
-	notes = fields.Text('Notes')
-
-	students = fields.One2many(comodel_name="ims.student", inverse_name="group", string="Students")
+	study = fields.Many2one(string="Study", comodel_name="ims.study", required="true")	
+	students = fields.One2many(string="Students", comodel_name="ims.student", inverse_name="group")
+	
+	@api.depends("study.acronym", "course", "acronym")
+	def _compute_name(self):
+		for rec in self:
+			rec.name = '%s%s%s' % (rec.study.acronym, rec.course, rec.acronym)
