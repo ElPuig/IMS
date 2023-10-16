7d74f80045d5fab619b94171977c7762bbe22915
FherStk <porrino.fernando@gmail.com>
2023-10-16 11:08:57 +0000

    New model for administrative and services person

---
+++ models/administrative.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ims_administrative(models.Model):
+	_name = "ims.administrative"
+	_description = "Administrative person: Collects personal and corporative data."
+	_inherit = "ims.corporate_person"
+    
+	type = fields.Selection(string='Type', object=[('secretary', 'Secretary'), ('concierje', 'Concierje')])
