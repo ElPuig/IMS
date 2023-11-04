97ce06d46244d437ff17f543e3ce7b1b8401957a
FherStk <porrino.fernando@gmail.com>
2023-11-04 11:14:34 +0000

    Company data

--- demo.sh
+++ demo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Installing the IMS (with demo data)..."
 sudo service odoo stop
+pip3 install phonenumbers
 sudo -u odoo bash -c 'odoo -d ims --stop-after-init -i ims -c /etc/odoo/odoo.conf'
 sudo service odoo start
 echo "Done!"
