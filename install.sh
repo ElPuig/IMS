c4c5d92696f43a13d882a47b257fbb36f3843880
FherStk <porrino.fernando@gmail.com>
2023-11-04 15:13:01 +0000

    removed phonenumbers installation, must be done as a prerequisite.

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Installing the IMS..."
 sudo service odoo stop
-pip3 install phonenumbers
 sudo -u odoo bash -c 'odoo -d ims --stop-after-init -i ims -c /etc/odoo/odoo.conf --without-demo=WITHOUT_DEMO'
 sudo service odoo start
 echo "Done!"
