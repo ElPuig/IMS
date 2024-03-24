94d4216b6f8a16d56b132d97682e5234141a00bb
FherStk <porrino.fernando@gmail.com>
2024-03-24 12:56:35 +0000

    Subject within subject forms behaviour have been improved!

--- models/curriculum/subject.py
+++ models/curriculum/subject.py
@@ -10,21 +10,20 @@ class ims_subject(models.Model):
     code = fields.Char(string="Code", required="true")
     acronym = fields.Char(string="Acronym", required="true")
     name = fields.Char(string="Name", required="true")
+    level = fields.Integer(string="Level")  # Note: this field is computed, but marking as compute needs Store=true for filtering and then is not beeing updated :(
     notes = fields.Text("Notes")
 
     study_id = fields.Many2one(string="Study", comodel_name="ims.study", required="true")
     teacher_id = fields.Many2one(string="Teacher", comodel_name="hr.employee", domain="[('employee_type', '=', 'teacher')]")
 
-    subject_ids = fields.One2many(string="Content", comodel_name="ims.subject", inverse_name="subject_id", domain="[('id', '!=', id)]")
-    subject_id = fields.Many2one(string="Main subject", comodel_name="ims.subject")
-
-    # TODO: the study_id is the parent's one when level > 1
+    # TODO: some way to directly open the new form instead of searching (no search is needed, no orphan should be allowed)
+    subject_ids = fields.One2many(string="Content", comodel_name="ims.subject", inverse_name="subject_id", domain="[('id', '!=', id), ('level', '<', level)]")
+    subject_id = fields.Many2one(string="Main subject", comodel_name="ims.subject")    
     
-    level = fields.Integer(string="Level", compute="_level")	
-
-    @api.depends("subject_id")
-    def _level(self):			            
+    @api.onchange("subject_id")
+    def _onchange_subject_id(self):
         for rec in self:
+            rec.study_id = rec.subject_id.study_id
             rec.level = rec.subject_id.level + 1              
 
     def name_get(self):
