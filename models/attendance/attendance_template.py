8ae6bf06cfbff4f2b5b9d6f89bf340067649d024
FherStk <porrino.fernando@gmail.com>
2024-10-18 09:24:54 +0000

    Fields filtered

--- models/attendance/attendance_template.py
+++ models/attendance/attendance_template.py
@@ -7,16 +7,16 @@ class ims_attendance_template(models.Model):
 	_description = 'Attendance template: contains the basic attendance data (who teaches what, where and for whom)'
 
 	# TODO: add a start_date and an end_date. It will be used to select the current data when creating a new session.
-	start_date = fields.Datetime(string="Start date")
-	end_date = fields.Datetime(string="End date")
+	start_date = fields.Datetime(string="Start date", required=True)
+	end_date = fields.Datetime(string="End date", required=True)
 	color = fields.Integer(string='Color', help='Field to store the color that will be used for calendar view')   
 
-	teacher_id = fields.Many2one(string="Teacher", comodel_name="hr.employee", domain="[('employee_type', '=', 'teacher')]")
-	level_id = fields.Many2one(string="Level", comodel_name="ims.level")
-	study_id = fields.Many2one(string="Study", comodel_name="ims.study") 			# TODO: filter by level
-	group_id = fields.Many2one(string="Group", comodel_name="ims.group") 			# TODO: filter by study
-	subject_id = fields.Many2one(string="Subject", comodel_name="ims.subject") 	# TODO: filter by group
-	space_id = fields.Many2one(string="Space", comodel_name="ims.space")			# TODO: autofill by group + subject | allow changes	
+	teacher_id = fields.Many2one(string="Teacher", comodel_name="hr.employee", domain="[('employee_type', '=', 'teacher')]", required=True)
+	level_id = fields.Many2one(string="Level", comodel_name="ims.level", required=True)
+	study_id = fields.Many2one(string="Study", comodel_name="ims.study", domain="[('level_id', '=', level_id)]", required=True)
+	group_id = fields.Many2one(string="Group", comodel_name="ims.group", domain="[('study_id', '=', study_id)]", required=True)
+	subject_id = fields.Many2one(string="Subject", comodel_name="ims.subject", domain="[('study_id', '=', study_id)]", required=True)
+	space_id = fields.Many2one(string="Space", comodel_name="ims.space", required=True)
 	
 	attendance_schedule_ids = fields.One2many(string="Sessions", comodel_name="ims.attendance_schedule", inverse_name="attendance_template_id")		
 	
@@ -29,3 +29,8 @@ class ims_attendance_template(models.Model):
 			result.append((rec.id, "%s (%s)" % (rec.subject_id.name_get()[0][1], rec.group_id.name)))			
 			
 		return result
+
+	@api.onchange('group_id')	
+	def _onchange_group_id(self):		
+		for rec in self:
+			rec.space_id = rec.group_id.space_id
