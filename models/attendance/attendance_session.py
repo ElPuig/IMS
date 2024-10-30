6cd69f9001ea0626e9ad1a0704f0d6eb5e2e3c58
FherStk <porrino.fernando@gmail.com>
2024-10-30 14:06:27 +0000

    Cleanup

--- models/attendance/attendance_session.py
+++ models/attendance/attendance_session.py
@@ -11,7 +11,6 @@ from .attendance_schedule import ims_attendance_schedule
 class ims_attendance_session(models.Model):
 	_name = "ims.attendance_session"
 	_description = "Attendance session: contains the data about every session done with the students."		
-	#_display_warning = fields.Boolean(compute="_default_display_warning", store=False)	
 	_display_warning = fields.Boolean(default=lambda self: self._default_display_warning(), store=False)	
 	
 	# NOTE: This is an statistical data model, should be unaltered if master-data changes, so the parent data will be copied.		
@@ -133,7 +132,6 @@ class ims_attendance_session(models.Model):
 				students.append([0, 0, {
 					"student_id": student
 				}])	
-
 			self.write({"attendance_status_ids": students})
 
 	def name_get(self):
@@ -141,7 +139,6 @@ class ims_attendance_session(models.Model):
 		result = []	
 		for rec in self:
 			result.append((rec.id, "%s | %s" % (rec.attendance_schedule_id.name_get()[0][1], rec.date)))			
-			
 		return result
 
 
@@ -150,81 +147,4 @@ class ims_attendance_session(models.Model):
 		local = pytz.timezone(user_time_zone) 
 		start_date = local.localize(local_date, is_dst=None) # start_date is a naive datetime 
 		start_date = start_date.astimezone(pytz.utc) 
-		
 		return datetime(start_date.year, start_date.month, start_date.day, start_date.hour, start_date.minute, 0, tzinfo=None)
-
-
-
-
-
-
-
-
-	# #name =fields.Char(string="Name", compute="_compute_name")
-	# # date = fields.Datetime(string="Date", default=fields.Datetime.now)
-	# # weekday = fields.Selection(string="Weekday", related="attendance_group.weekday") #, store=True)
-	# # start_time = fields.Float(string="Start Time", related="attendance_group.start_time") #, store=True)
-	# # end_time = fields.Float(string="End Time", related="attendance_group.end_time") #, store=True)
-	# # start_date = fields.Datetime(string="Start date", compute="_compute_start_date", readonly=False, store=True)
-	# # TODO: enddate
-	# duration = fields.Integer(string="Duration", compute="_compute_duration", readonly=False, store=True)
-
-	# notes = fields.Text(string="Notes")
-	# attendance_group = fields.Many2one(string="Attendance Group", comodel_name="ims.attendance_group")
-
-	# attendance_statuses = fields.One2many(string="Student status", comodel_name="ims.attendance_status", inverse_name="attendance_session")		
-	# has_statuses = fields.Boolean(compute="_compute_has_statuses", store=False)	
-	# # event_color = fields.Integer("Color", compute="computeColor")
-
-	# # def computeColor(self):
-	# # 	for record in self:
-	# # 		record.name = record.attendance_group.color
-	
-	# def _compute_name(self):
-	# 	for record in self:
-	# 		record.name = record.attendance_group.subject.acronym
-
-	# @api.depends("date", "start_time")
-	# def _compute_start_date(self):			
-	# 	for rec in self:
-	# 		#Computing the start date
-	# 		start_time = math.modf(rec.start_time)				
-	# 		local_time = datetime(rec.date.year, rec.date.month, rec.date.day, int(start_time[1]), int(start_time[0]*10), 0)
-	# 		rec.start_date = self.convert_to_utc_date(local_time)
-
-
-	# # @api.depends("date")
-	# # def _compute_date(self):
-	# # 	for record in self:
-	# # 		record.start_date = record.date
-	# # 		record.end_date = record.date
-
-	# def convert_to_utc_date(self, local_date):
-	# 	user_time_zone = self.env.context["tz"] # can be fetched form logged in user if it is set 
-	# 	local = pytz.timezone(user_time_zone) 
-	# 	start_date = local.localize(local_date, is_dst=None) # start_date is a naive datetime 
-	# 	start_date = start_date.astimezone(pytz.utc) 
-		
-	# 	return datetime(start_date.year, start_date.month, start_date.day, start_date.hour, start_date.minute, 0, tzinfo=None)
-
-	# @api.depends("attendance_statuses")
-	# def _compute_has_statuses(self):
-	# 	for record in self:
-	# 		record.has_statuses = len(record.attendance_statuses) > 0
-
-	# def generate_statuses_by_templates (self):
-	# 	for contact in self.attendance_group.attendance_templates.student:
-	# 		status = self.env["ims.attendance_status"].create({
-	# 			"attendance_session": self.id,
-	# 			"student" : contact.id,
-	# 			"status" : "1"
-	# 		})
-
-	# def generate_statuses (self, session):
-	# 	for contact in self.attendance_group.attendance_templates.student:
-	# 		status = self.env["ims.attendance_status"].create({
-	# 			"attendance_session": self.id,
-	# 			"student" : contact.id,
-	# 			"status" : "1"
-	# 		})
-			
--- models/attendance/attendance_status.py
+++ models/attendance/attendance_status.py
@@ -14,36 +14,3 @@ class ims_attendance_status(models.Model):
 	attendance_session_id = fields.Many2one(string="Session", comodel_name="ims.attendance_session")
 	notes = fields.Text("Notes")
 	
-	
-	# notes = fields.Text("Notes")
-	# #TODO revisar si en lugar de un Selection debe ser un modelo status 
-	# # many2many para permitir varias entradas en una misma sesión (por ejemplo incidencias)
-
-	# #Status constants
-	# CONS_ATTENDED="1"
-	# CONS_MISS="2"
-	# CONS_DELAY="3"
-
-	# status = fields.Selection(
-    #     [(CONS_ATTENDED, "Attended"), (CONS_MISS, "Miss"), (CONS_DELAY, "Delay")],
-    #     string="Status",
-    #     default="1",
-    #     required=True,
-    # )
-
-	# attendance_session = fields.Many2one(string="Session", comodel_name="ims.attendance_session")
-	# student = fields.Many2one(comodel_name="res.partner", string="Student", domain = "[("contact_type","=","student")]")
-
-	# def action_miss(self):
-	# 	self.ensure_one()
-	# 	self.status = self.CONS_MISS
-
-	# def action_attend(self):
-	# 	self.ensure_one()
-	# 	self.status = self.CONS_ATTENDED
-
-	# def action_delay(self):
-	# 	self.ensure_one()
-	# 	self.status = self.CONS_DELAY
-
-	
