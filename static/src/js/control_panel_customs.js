d7212298ade55d939f9e7a0d4a1b19098c99fab5
FherStk <porrino.fernando@gmail.com>
2024-12-07 17:38:24 +0000

    comment

--- static/src/js/control_panel_customs.js
+++ static/src/js/control_panel_customs.js
@@ -12,20 +12,20 @@ patch(ControlPanel.prototype, "control_panel_customs", {
         owl.onRendered(() => {                 
             //The "Subject" breadcrum dissapears when jumping to another models, just like when refreshig and jumped from model to model using a button...
             //TODO: try to avoid missing the breadcrums on refreshing after pushing a button (through python action).    
-            // var model = this.env.searchModel.resModel;        
-            // if (this.env.config.viewType == "form" && this.breadcrumbs.length == 1 && model.startsWith("ims.")){
-            //     //Workaroung to add the missing level-1 breadcrum, should be properly fixed.                                 
-            //     var br = $(this.root.el).find("ol.breadcrumb");
-            //     var am = ActionService.start(this.env);                
-            //     console.log(this.env.searchModel.context);
+            var model = this.env.searchModel.resModel;        
+            if (this.env.config.viewType == "form" && this.breadcrumbs.length == 1 && model.startsWith("ims.")){
+                //Workaroung to add the missing level-1 breadcrum, should be properly fixed.                                 
+                var br = $(this.root.el).find("ol.breadcrumb");
+                var am = ActionService.start(this.env);                
+                console.log(this.env.searchModel.context);
 
-            //     if($(br).children("li").length == 1){                                                        
-            //         this.prependToBreadcrums(am, model, br);                           
-            //     }
-            //     else if (this.env.searchModel.context["subject_view_list"] == true){
-                    
-            //     }
-            // }            
+                if($(br).children("li").length == 1){                                                        
+                    this.prependToBreadcrums(am, model, br);                           
+                }
+                else if (this.env.searchModel.context["subject_view_list"] == true){
+                    //TODO: i'm not sure if worths it...    
+                }
+            }            
         });
     },
     
