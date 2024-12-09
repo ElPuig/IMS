bc0dedb2c8cbe19bcf6a0ea6db78b034a32ea32f
FherStk <porrino.fernando@gmail.com>
2024-12-09 15:38:48 +0000

    Removed unnecesary file

--- debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting the IMS [DEBUG MODE]..."
-sudo service odoo stop
-sudo -u odoo bash -c 'odoo -d ims --stop-after-init -c /etc/odoo/odoo.conf --dev=all'
-sudo service odoo start
